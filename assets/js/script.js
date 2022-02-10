316cf130fe277ef072874ea674d185b7490956b9
hermani456 <hermani457@gmail.com>
2022-02-10 16:08:44 -0300

    eliminado codigo js inecesario

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,6 @@
 $(document).ready(function(){
    // habilita los tooltips
    $('[data-toggle="tooltip"]').tooltip()
-   // muestra una alerta al presionar el boton enviar correo
-   $('#enviarCorreo').click(function(){
-   alert("El correo fue enviado correctamente.")
-   })
-   // cambia de color los h1 al hacerles doble click
-   $('h1').on('click', function(){
-   $(this).toggleClass('toggle-color');
-   })
-   // esconde el contenido de las cards
-   $('.hh5').click(function(){
-   $('.cards').toggle();
-   })
 
    $('a').click(function(){
       var gato = this.hash
