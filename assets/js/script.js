1f6555c00e98f038c97d29bf9fddc952b90430e5
hermani456 <hermani457@gmail.com>
2022-02-10 23:53:46 -0300

    agregado boton con popover en html y activado de popover en js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
    // habilita los tooltips
    $('[data-toggle="tooltip"]').tooltip()
+   $('[data-toggle="popover"]').popover()
    // smooth scroll
    $('a').click(function(){
       var gato = this.hash
